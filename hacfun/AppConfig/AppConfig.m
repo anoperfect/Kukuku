d3eaf79d6384e0b15e662419b80a4e8077fcb198
Ben <Ben@bendeair.zte.com.cn>
2016-05-31 11:28:10 +0800

    *.appkey store assigned appsecret on locale.

--- hacfun/AppConfig/AppConfig.m
+++ hacfun/AppConfig/AppConfig.m
@@ -9,8 +9,18 @@
 #import "AppConfig.h"
 #import "FMDB.h"
 #import "FuncDefine.h"
-#import "appkey.h"
 
+
+
+
+
+//这是Ku岛管理分配的appkey.
+#define APPKEY          @"ee2a5472-065b-41c2-8b8c-d4cd56abcdef"
+
+//这不是正式的appSecret. appSecret需保密. 正式的appSecret存本地用于test&build.
+#define APPSECRET       @"s6Q1w62100abcdef"
+
+#import "kukuku.appkey"
 
 
 #define DBNAME_CONFIG           @"config"
